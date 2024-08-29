6e48407b8810604434502cf12558ac2c834fe3ff
Mike De Leon <mdeleon@horizonmedia.com>
2024-08-29 12:42:04 -0400

    testing components, created a folder for them and added a test

---
+++ Sports_Project/best-players-ever/src/Components/Personal-Info.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function PersonalDetails(){
+
+    return(
+        <div>
+            <p>This is where the personal details will appear</p>
+        </div>
+    )
+
+}
