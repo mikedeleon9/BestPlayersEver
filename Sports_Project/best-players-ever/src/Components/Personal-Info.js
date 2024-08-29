c5516d26449d9c592c886d0d7d062315c8fa4edf
Mike De Leon <mdeleon@horizonmedia.com>
2024-08-29 15:30:10 -0400

    adding tailwind to the project

--- Sports_Project/best-players-ever/src/Components/Personal-Info.js
+++ Sports_Project/best-players-ever/src/Components/Personal-Info.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./components.css" 
 
 export default function PersonalDetails(props){
 
     return(
-        <div>
+        <div className="text-red-600">
         <p>{props.name}</p>
         <p>{props.birthPlace}</p>
         <img src={props.img} />
