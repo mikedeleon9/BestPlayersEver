b1417c1bcecd62653526ace81a6c59c3309c5f88
mikedeleon9 <mikedeleon9@gmail.com>
2024-08-28 21:48:50 -0400

    Adding stats.js

---
+++ Sports_Project/best-players-ever/src/Assets/stats.js
@@ -0,0 +1,57 @@
+const playerInfo = [{
+    personalDetails: {
+      name: "Sammy Sosa",
+      birthPlace: "San Pedro de Macoris",
+      teams: ["Rangers", "White Sox", "Cubs", "Orioles"]
+    },
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp2rTzpjdLPAmO3KhLc6KX_lScDBENbK4jWw&s",
+    position: "Outfielder",
+    stats:{
+      homeruns : 609,
+      avg : .273,
+      obp : .344,
+      slg : .534,
+      ops : .878,
+      opsPlus : 128
+    },
+    accomplishments : [
+      "1998 NL MVP", "7X All-Star", "6X Silver Slugger", "HR Derby Champ"
+    ],
+    trivia: "Only Player in MLB History with three 60 HR seasons"
+  },
+
+
+
+{
+    personalDetails: {
+        name: "Albert Pujols",
+        birthPlace: "Santo Domingo",
+        teams: ["Cardinals", "Angels", "Dodgers"]
+    },
+    position: "First Base",
+    stats: {
+        homeruns: 703,
+        avg: .296,
+        obp: .374,
+        slg: .544,
+        ops: .918,
+        opsPlus: 145
+    },
+    accomplishments : [
+        "3X MVP (05, 08, 09", "11X All-Star", "6X Silver Slugger", "Rookie of The Year", "2X World Series"
+    ],
+    trivia: "Grounded into the most double plays in MLB History"
+},
+
+
+{
+    personalDetails: {
+        name: "Adrian Beltre"
+    }
+
+
+
+
+}
+
+    ]
