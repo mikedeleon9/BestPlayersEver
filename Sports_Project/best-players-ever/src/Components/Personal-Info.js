df3f0b4897c9d76d7cd4416fb2410a8fd408f898
mikedeleon9 <mikedeleon9@gmail.com>
2024-08-30 12:59:59 -0400

    adding css

--- Sports_Project/best-players-ever/src/Components/Personal-Info.js
+++ Sports_Project/best-players-ever/src/Components/Personal-Info.js
@@ -4,11 +4,14 @@ import "./components.css"
 export default function PersonalDetails(props){
 
     return(
-        <div className="inline-block ">
-        <p className="text-blue-500 text-3xl">{props.name} </p>
+        <div className="flex border-2 border-white background-blue">
         
-        <img className="rounded-full" src={props.img} />
-        <p className="text-blue-500">{props.birthPlace}</p>
+          
+          <img className="rounded-full border-2 border-white" src={props.img} />
+          <div>
+          <p className="text-white text-5xl">{props.name} </p>
+          <p className="text-white">{props.birthPlace}</p>
+          </div>
       </div>
     )
 
