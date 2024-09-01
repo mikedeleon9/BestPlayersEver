53d2a6d92debe049d798e58096bda663eab5c2dd
unknown <mikedeleon9@gmail.com>
2024-09-01 13:32:45 -0400

    added some columns and adjusted the height

--- Sports_Project/best-players-ever/src/App.js
+++ Sports_Project/best-players-ever/src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 import './App.css';
 import stats from "./Assets/stats";
 import PersonalDetails from  "./Components/Personal-Info";
 import CareerStats from "./Components/Career-Stats";
 
-function App() {
-  
- 
 
+function App() {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+ 
+  function nextPlayer() {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % stats.length); // Loop back to 0 if you reach the end
+  }
     return (
      
         <div className="App">
           <PersonalDetails 
-            details = {stats[0].personalDetails}
-          
-          
-        
+            details = {stats[currentIndex].personalDetails}
           />
           <CareerStats 
-         statistics = {stats[0].CareerStats}
+         statistics = {stats[currentIndex].CareerStats}
+         mainTeam = {stats[currentIndex].personalDetails.mainTeam}
           />
+          <button onClick = {nextPlayer} className="button">Next</button>
         </div>
       
     );
--- Sports_Project/best-players-ever/src/Components/Career-Stats.js
+++ Sports_Project/best-players-ever/src/Components/Career-Stats.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./components.css" 
 import { teamBackgroundColors } from "../Assets/teamDetails";
-import stats from "../Assets/stats";
 
-export default function CareerStats({statistics}){
+
+export default function CareerStats({statistics, mainTeam}){
     
     const {avg, obp, slg, ops, opsPlus} = statistics;
-    const mainTeam = stats[0].personalDetails.mainTeam;
     const backgroundColor = teamBackgroundColors[mainTeam];
 
     return(
-        <div className={`w-1/2 h-48 flex items-center relative ${backgroundColor} rounded-3xl border-2 border-white`}>
+        <div className={`w-1/2 grid grid-cols-5 ${backgroundColor} rounded-3xl border-2 border-white`}>
            <p>AVG: {avg}</p>
             <p>OBP: {obp}</p>
             <p>SLG: {slg}</p>
