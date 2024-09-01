046884ebb266a4cc04ddac78e8dbcfcf8bc28f20
unknown <mikedeleon9@gmail.com>
2024-09-01 15:14:34 -0400

    added function for ps

--- Sports_Project/best-players-ever/src/Components/Career-Stats.js
+++ Sports_Project/best-players-ever/src/Components/Career-Stats.js
@@ -3,18 +3,16 @@ import "./components.css"
 import { teamBackgroundColors } from "../Assets/teamDetails";
 
 
-export default function CareerStats({statistics, mainTeam}){
+export default function CareerStats({statistics: { avg, obp, slg, ops, opsPlus }, mainTeam}){
     
-    const {avg, obp, slg, ops, opsPlus} = statistics;
+    const statsArray = { AVG: avg, OBP: obp, SLG: slg, OPS: ops, "OPS+": opsPlus };
     const backgroundColor = teamBackgroundColors[mainTeam];
 
     return(
         <div className={`w-1/2 p-5 grid grid-cols-5 ${backgroundColor} rounded-3xl border-2 border-white text-white`}>
-           <p className="text-lg font-bold">AVG: <span className="text-base font-normal">{avg}</span></p>
-            <p>OBP: <span>{obp}</span></p>
-            <p>SLG: <span>{slg}</span></p>
-            <p>OPS: <span>{ops}</span></p>
-            <p>OPS+: <span>{opsPlus}</span></p>
+           {Object.entries(statsArray).map(([key, value]) => (
+            <p key={key}>{key}: {value}</p>
+           ))}
         
         </div>
     )
