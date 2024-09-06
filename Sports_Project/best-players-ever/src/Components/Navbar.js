1f36e531bc87f25f10295fc117a29a88686963f2
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-06 11:10:13 -0400

    adding style to home button

--- Sports_Project/best-players-ever/src/Components/Navbar.js
+++ Sports_Project/best-players-ever/src/Components/Navbar.js
@@ -7,8 +7,11 @@ export default function Navbar({mainTeam}){
     const backgroundColorClass = teamBackgroundColors[mainTeam];
     const borderColorClass = teamBorderColors[mainTeam];
     return(
-        <div className= {`border-2 ${borderColorClass} w-48 ${backgroundColorClass} h-96 flex align-center rounded-3xl p-5`}>
-            <img className="w-4 h-4" src={HomeIcon} alt="home-icon"></img>
+        <div className= {`border-2 ${borderColorClass} w-48 ${backgroundColorClass} h-96 flex flex-col align-center rounded-3xl p-5`}>
+            <div className="flex items-center gap-2">
+                <img className="w-4 h-4" src={HomeIcon} alt="home-icon"></img>
+                <p className="text-white">Home</p>
+            </div>
         </div>
     )
 }
