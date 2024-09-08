d73eca08057a2708a190c2552d046c17646fb16d
unknown <mikedeleon9@gmail.com>
2024-09-07 22:01:56 -0400

    \adding

--- Sports_Project/best-players-ever/src/Components/Navbar.js
+++ Sports_Project/best-players-ever/src/Components/Navbar.js
@@ -13,7 +13,7 @@ export default function Navbar() {
           <div className="w-full mr-40 flex justify-end items-center ">
           
             <input 
-              className="bg-gray-700 relative w-full text-white border-none rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-600" 
+              className="bg-gray-700 placeholder:ml-4 relative w-full text-white border-none rounded-full p-2 focus:outline-none focus:ring-2 focus:ring-blue-600" 
               placeholder="Search for a Player"
              
             />
