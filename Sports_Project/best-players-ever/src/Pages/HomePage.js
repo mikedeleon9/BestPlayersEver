05ecce151ad53f9d1139c2a4f3620586067f6b31
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-13 13:43:23 -0400

    updating homepage

--- Sports_Project/best-players-ever/src/Pages/HomePage.js
+++ Sports_Project/best-players-ever/src/Pages/HomePage.js
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 export default function HomePage(){
     return(
         <div className="">
+            <div className="">
           <p>I have decided to rank the best 25 players that have come out of the Dominican Republic. This is a tough exercise, but a rewarding one, as it makes me appreciate
             all of the greatness that has hailed our of my country.
           </p>
-            
+            </div>
         </div>
     )
 }
