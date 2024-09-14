fce7ad0c3bb15b72320134f1ad5f44ae13b3ff8c
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-13 23:53:42 -0400

    adding more to the rank bar

--- Sports_Project/best-players-ever/src/Components/RankBar.js
+++ Sports_Project/best-players-ever/src/Components/RankBar.js
@@ -4,8 +4,11 @@ import "./components.css";
 export default function RankBar(){
 
     return(
-        <div className="w-full h-12 top-0 sticky border-2 p-4">
-            <p>Ranks</p>
+        <div className="w-full h-12 top-0 sticky  p-4 flex items-center gap-4">
+            <p>Ranks</p> 
+            <p>15-11</p>
+            <p>10-6</p>
+            <p>5-1</p>
         </div>
 
 
