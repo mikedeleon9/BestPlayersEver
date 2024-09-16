9db2cc2d5dc93cdec62e5b4158442bea8e3e1159
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-16 08:07:47 -0400

    adding padding to homepage

--- Sports_Project/best-players-ever/src/Pages/HomePage.js
+++ Sports_Project/best-players-ever/src/Pages/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function HomePage(){
     return(
         <div className="">
-            <div className="">
+            <div className="p-4">
           <p>I have decided to rank the best 25 players that have come out of the Dominican Republic. This is a tough exercise, but a rewarding one, as it makes me appreciate
             all of the greatness that has hailed our of my country.
           </p>
