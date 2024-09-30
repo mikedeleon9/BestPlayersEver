88c5e358635a0aef5a446ea32ff4755aa4ad5a28
mikedeleon9 <mikedeleon9@gmail.com>
2024-09-30 16:20:28 -0400

    starting trivia page

--- Sports_Project/best-players-ever/src/Pages/Trivia.js
+++ Sports_Project/best-players-ever/src/Pages/Trivia.js
@@ -3,6 +3,9 @@ import React from "react";
 export default function TriviaPage(){
 
     return(
-        <div></div>
+        <div>
+
+            <h1>This is going to be the trivia page</h1>
+        </div>
     )
 }
