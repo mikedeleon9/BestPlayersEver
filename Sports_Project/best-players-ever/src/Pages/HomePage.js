6f197d044f4894aeae3e85627a7e9974ccf56467
mikedeleon9 <mikedeleon9@gmail.com>
2024-11-08 12:05:12 -0500

    messing around with opacity

--- Sports_Project/best-players-ever/src/Pages/HomePage.js
+++ Sports_Project/best-players-ever/src/Pages/HomePage.js
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import bautistaFlip from "../Assets/Images/BautistaFlip.jpg"
 export default function HomePage(){
     return(
-        <div className="overflow-hidden w-screen h-screen ">
-          <div className="flex ">
-           <img className="w-full  object-cover object-top opacity-20" src={bautistaFlip} ></img>
-           </div>
+        <div className="overflow-hidden w-screen h-screen relative" >
+          <div className="details h-full " style={{ backgroundImage: `url(${bautistaFlip})` }}>
+          <div className="absolute inset-0 bg-black opacity-70"></div>
+          {/*Main Content */}
+          <div className="relative z-10">
+            <h2>Jose Bautista</h2>
+            </div>
+          </div>
+         
         </div>
     )
 }
