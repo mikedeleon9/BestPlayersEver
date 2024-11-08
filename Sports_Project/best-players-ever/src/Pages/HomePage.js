36f94ef551415acb5eb2dcfa9acb11186f83175b
mikedeleon9 <mikedeleon9@gmail.com>
2024-11-08 11:45:27 -0500

    still trying things

--- Sports_Project/best-players-ever/src/Pages/HomePage.js
+++ Sports_Project/best-players-ever/src/Pages/HomePage.js
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import bautistaFlip from "../Assets/Images/BautistaFlip.jpg"
 export default function HomePage(){
     return(
-        <div className=" w-screen h-96 overflow-hidden opacity-30 ">
-           <img className="border-2" src={bautistaFlip}></img>
+        <div className="overflow-hidden w-screen h-screen ">
+          <div className="flex ">
+           <img className="w-full  object-cover object-top opacity-20" src={bautistaFlip} ></img>
+           </div>
         </div>
     )
 }
